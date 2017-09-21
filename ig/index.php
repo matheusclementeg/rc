712760c70f9a8a4248a7fb87f61d29f1ab8d16b0
Pedro Kfuri <pedrokfuri@outlook.com>
2017-09-21 11:10:59 -0300

    updated starts and stops, final video

--- ig/index.php
+++ ig/index.php
@@ -1,5 +1,7 @@
-<html lang="pt_BR" class=" js flexbox flexboxlegacy canvas canvastext webgl no-touch geolocation postmessage websqldatabase indexeddb hashchange history draganddrop websockets rgba hsla multiplebgs backgroundsize borderimage borderradius boxshadow textshadow opacity cssanimations csscolumns cssgradients cssreflections csstransforms csstransforms3d csstransitions fontface generatedcontent video audio localstorage sessionstorage webworkers applicationcache svg inlinesvg smil svgclippaths"><head>
-	
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Entenda o que é o Marketing de Conteúdo a partir desse super infográfico interativo!">
@@ -16,115 +18,17 @@
 	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
 	<link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.min.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
-	<!-- <link href="css/jquery.notifyBar.css" rel="stylesheet"> -->
 	<link href="css/loaders.min.css" rel="stylesheet">
-	<link href="css/main.css" rel="stylesheet">
-	<!--<script async="" src="//www.google-analytics.com/analytics.js"></script>-->
+	<link href="css/custom.css" rel="stylesheet">
 	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
 </head>
-<body cz-shortcut-listen="true">
-	<!--<div class="sr-only">
-		<section>
-			<h1>THE SCIENCE BEHIND WHY NATIVE ADS WORK</h1>
-			<p>Which part of the brain are you marketing to?</p>
-			</section>
-		<section>
-			<h1>WE ARE LIVING IN THE MOBILE AGE</h1>
-			<p>The proliferation of smartphones and tablets has had a profound change on the way people consume content.</p>
+<body>
 
-			<p><strong>There is a near infinite amount of content, but only a finite amount of time to consume it all.</strong></p>
-
-			<p>As a result, even the highest-performing mobile ads have click through rates of 1-5%.</p>
-			<p>Meaning those ads fail to convert the other 95-99%, which could amount to millions of unconverted impressions over the course of a campaign.</p>
-
-			<p>Understanding the value of the impression can help advertisers build their brand</p>
-			<ul>
-				<li>Most marketers only tap into 5% of our brain - the conscious part</li>
-				<li>But, it's the 95% - the non-conscious part - that is responsible for driving our choices and actions.</li>
-			</ul>
-			<p>For that we turn to Neuroscience. Neuroscientists believe our subconscious is the motivating force behind many of our actions, including which brands we buy from.</p>
-		</section>
-		<section>
-			<h1>WE ARE IN THE MIDST OF A READING REVOLUTION</h1>
-			<p>The modern internet is all feeds, especially on Mobile and we’re having to read more than ever before.</p>
-			<p>We read status messages on FB, Tweets, messages, instagram comments all day, every day. Therefore, people spend more time browsing online than engaging with content. </p>
-		</section>
-		<section>
-			<h1>THE SCIENCE BEHIND NATIVE ADS</h1>
-			<p>Neuroscience reveals that people see banner ads, but they truly read native ads.</p>
-			<p>Native advertising is NOT just about placement, it’s about being native to user expectations and experience of seeking relevant content through reading.</p>
-		</section>
-		<section>
-			<h1>THE NATIVE AD VALUE</h1>
-			<p>NATIVE ADS TAP INTO MORE BRAIN, DELIVER MORE FOCUS, MORE TIME, MORE MEANING, MORE SUBCONSCIOUS IMPACT, MORE CONSCIOUS IMPACT</p>
-			<ul>
-				<li>Native Ads result in 2X more Visual Focus than banners and are read in the same way as editorial headlines.</li>
-				<li>Reading a Native Ad headline yields 308 X more time ( 4000ms ) of consumer attention than processing an image or banner (13ms).</li>
-				<li>Native ads registered a 9% higher lift in brand affinity and an 18% higher lift for purchase intent.</li>
-			</ul>
-			<p><strong>Native ads are effective because they require the use of the entire brain.</strong></p>
-			<p>Unlike banner ads, native ads require the use of both hemispheres of the brain. This results in great engagement, retention, and action.</p>
-			<table>
-				<thead>
-					<tr>
-						<th>Left Brain</th>
-						<th>Right Brain</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>Text processing</td>
-						<td>Image processing</td>
-					</tr>
-					<tr>
-						<td>Single task focused</td>
-						<td>Multi-tasked focused</td>
-					</tr>
-					<tr>
-						<td>Literal</td>
-						<td>Metaphorical</td>
-					</tr>
-				</tbody>
-			</table>
-			<p>The act of reading a native ad triggers neural pathways in the brain and unravels a tapestry of associations in the mind.</p>
-			<p>Reading a native ad is able to neurologically change the brain by creating associations.</p>
-			<p>Triggering subconscious associations is able to influence message perception.</p>
-		</section>
-		<section>
-			<h1>THE SCIENCE OF SUBCONSCIOUS DECISION MAKING</h1>
-			<p>People are biologically wired to make split second subconscious, snap judgments about the world → the same few seconds it takes to read a native ad.</p>
-		</section>
-		<section>
-			<h1>A NATIVE AD INSTANT IMPRESSION HAS INTRINSIC VALUE:</h1>
-			<p>The impression is the point of influence - make a good impression.</p>
-			<p>A few seconds with a meaningful thumbnail &amp; headline can help inform our long-term beliefs, help us jump to conclusions and make important associations with a brand.</p>
-			<p>Meaning those 99% of people who don't convert (or millions of unconverted impressions over the course of a campaign) are likely to have the same snap judgement as those who do and would result in a similar outlook towards the brand long term.</p>
-			<h2>OPTIMIZING THE VALUE OF THE NATIVE AD IMPRESSION</h2>
-			<p>Neuroscience reveals that optimizing the ad can increase a person’s interest in the message on a subconscious level and increase the value of the impression.</p>
-			<p><strong>Optimize Native Ad components to trigger memory, emotion and empathy.</strong></p>
-			<ul>
-				<li>Learn to write headlines that can increase a person's interest in the message on a subconscious level.</li>
-				<li>Learn to select thumbnails that increase trust in the message</li>
-				<li>Learn to navigate your role as a brand.</li>
-			</ul>
-		</section>
-	</div>-->
 	<div id="container">
 		<video src="motion-infographic.mp4" preload="auto" poster="img/poster.gif" style="max-height: 446px;"></video>
 		<div id="loader" style="display: none;">
-			<div class="animation loader-inner ball-grid-pulse">
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-			</div>
-			<div class="text">Carregando</div>
+			<div class="text">Carregando...</div>
 		</div>
 		<a id="begin" class="fadeInUp animated">
 			<div class="text">Clique para começar</div>
@@ -140,108 +44,20 @@
 				<img src="img/next.png" width="65px">
 			</a>
 		</div>
-		<img id="mobile-infographic" src="img/static-infographic.jpg">
-		<!--<div class="banner" style="display: none;">
-			<img src="img/download-banner.png" alt="">
-			<a href="#" data-toggle="modal" data-target="#download_modal"></a>
-		</div>-->
-		<!--<a id="download-button" data-toggle="modal" data-target="#download_modal"></a>-->
-		<!--<div id="social-buttons">
-			<a data-share="facebook"><span class="icon icon-facebook"></span></a>
-			<a data-share="twitter"><span class="icon icon-twitter"></span></a>
-			<a data-share="linkedin"><span class="icon icon-linkedin"></span></a>
-		</div>-->
+		<img id="mobile-infographic" src="img/static-infographic.png">
 	</div>
-	<nav id="sidebar">
-		<header>
-			<h4>Capítulos</h4>
-		</header>
-		<ul class="menu-items">
-			<li><a href="#" data-chapter="0" data-scroll="0">Intro</a></li>
-			<li><a href="#" data-chapter="1" data-scroll="1920">Capítulo 1</a></li>
-			<li><a href="#" data-chapter="3" data-scroll="3740">Capítulo 2</a></li>
-			<li><a href="#" data-chapter="4" data-scroll="4830">Capítulo 3</a></li>
-			<li><a href="#" data-chapter="6" data-scroll="6920">Capítulo 4</a></li>
-			<li><a href="#" data-chapter="8" data-scroll="7845">Capítulo 5</a></li>
-			<li><a href="#" data-chapter="19" data-scroll="12760">Capítulo 6</a></li>
-		</ul>
-		<!--<footer>
-			<h5>Presented By</h5>
-			<a href="http://sharethrough.com/" target="_blank" class="logo">
-				<img src="img/logo-sharethrough.svg" alt="">
-			</a>
-			<a href="http://www.columnfivemedia.com" target="_blank" class="logo">
-				<img src="img/logo-c5.svg" alt="">
-			</a>
-		</footer>-->
-	</nav>
-	<a class="menu-toggle" href="#">
-		<span class="icon-bar">&nbsp;</span>
-		<span class="icon-bar">&nbsp;</span>
-		<span class="icon-bar">&nbsp;</span>
-	</a>
 	<div id="aspect-warning">
 		<p>Maximize o tamanho de sua tela para melhorar a experiência. Caso esteja usando um dispositivo móvel, tente girar a tela para o modo paisagem.</p>
 	</div>
 	<div id="height-warning">
 		<p>A altura do seu navegador é muito pequena. Maximize a janela para melhor experiência.</p>
 	</div>
-	<!--<div id="download_modal" class="modal fade" aria-hidden="true">
-		<div class="modal-dialog">
-			<form id="contact_form" class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-					<h4 class="modal-title">Download EBook!</h4>
-				</div>
-				<div class="modal-body">
-					<div class="container-fluid">
-						<div class="row">
-							<div class="form-group col-xs-12 col-sm-6">
-								<label for="first_name" class="control-label sr-only">First Name</label>
-								<input name="first_name" class="form-control" placeholder="First Name" required="">
-							</div>
-							<div class="form-group col-xs-12 col-sm-6">
-								<label for="last_name" class="control-label sr-only">Last Name</label>
-								<input name="last_name" class="form-control" placeholder="Last Name" required="">
-							</div>
-							<div class="form-group col-xs-12">
-								<label for="email_address" class="control-label sr-only">Email Address</label>
-								<input name="email_address" class="form-control" type="email" placeholder="Email Address" required="">
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="modal-footer">
-					<button type="submit" class="btn btn-primary">Download Free EBook</button>
-				</div>
-			</form>
-		</div>
-	</div>-->
-
-	<!-- <div id="debug" style="position:absolute;bottom:0;right:0">
-		<div id="currentTime"><strong>currentTime: </strong><span class="value" style="display:inline-block;width:100px"></span></div>
-	</div> -->
-
-	<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.1/jquery.scrollTo.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.imagesloaded/3.1.8/imagesloaded.pkgd.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.fileDownload/1.4.2/jquery.fileDownload.js"></script>
-	<!-- <script src="js/jquery.notifyBar.js"></script> -->
-	<script src="js/main.js"></script>
-
-	<script>
-	//(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	//(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	//m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	//})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-	//ga('create', 'UA-11201349-6', 'auto');
-	//ga('create', 'UA-10335640-19', 'auto', { 'name' : 'sharethrough'});
-	//ga('send', 'pageview');
-  	//ga('sharethrough.send', 'pageview');
-	</script>
+	<script src="js/custom.js"></script>
 
 </body>
 </html>
---
+++ ig/js/custom.js
@@ -0,0 +1,240 @@
+$(function () {
+	var chapters = [
+		{ start: 0, stop: 8.0 }, /*como era feito antes*/
+		{ start: 8.01, stop: 13.6 }, /*como funciona hoje*/
+		{ start: 13.61, stop: 16.6 }, /*conteudo = atrair pessoas certas*/
+		{ start: 16.61, stop: 22.59 }, /*3 circulos de motivos*/
+		{ start: 22.60, stop: 25.49 }, /*estrategico, direcionado e reduz riscos*/
+		{ start: 25.50, stop: 28.6 }, /*conteudo custa até % menos*/
+		{ start: 28.61, stop: 30.35 }, /*gera mais oportunidade de venda*/
+		{ start: 30.36, stop: 31.2 }, /*como funciona?*/
+		{ start: 31.21, stop: 35.25 }, /*ima e pneu*/
+		{ start: 35.26, stop: 37.1 }, /*eles se tornam clientes*/
+		{ start: 37.11, stop: 41.8 }, /*promotores da sua marca etc*/
+		{ start: 41.81, stop: 53.3 }, /*lucro economia clientes etc*/
+		{ start: 53.31, stop: 55.85 }, /*que tal comecar agora*/
+		{ start: 55.85, stop: 60.5 }, /*a rock content esta aqui pra ajudar*/
+		{ start: 60.5, stop: 70 }, /**/
+		{ start: 70.01, stop: 77 } /**/
+
+	];
+	var currentChapter = -1;
+	var video = $('#container video')[0],
+		$seekItems = $('[data-seek]').sort(function (a, b) {
+			return $(b).data('seek') - $(a).data('seek'); // Causes a reverse sort
+		}),
+		$chapterItems = $('[data-chapter]').sort(function (a, b) {
+			return $(b).data('chapter') - $(a).data('chapter');
+		});
+
+	function repositionElements() {
+		if(window.innerWidth < 1200) {
+			var w = $('#mobile-infographic').width(),
+				h = $('#mobile-infographic').height();
+			$('#download-button').css({
+				left: 0.58333333333333 * w,
+				top: 0.94985308521058 * h,
+				width: 0.225 * w,
+				height: 0.00914136467516 * h
+			});
+		}
+	}
+
+	function onResize() {
+		var win_h = $(window).height(),
+			win_w = $(window).width();
+
+		$('#container video').css({
+			'max-height': win_h
+		});
+		$('#container .poster').css({
+			width: $('#container video').width(),
+			height: $('#container video').height()
+		});
+
+		repositionElements();
+
+		if(win_w >= 1200) {
+			$('#container').scrollTo(0);
+
+			if(video.currentTime > 1.5) {
+				$('.banner').show();
+			} else {
+				$('.banner').hide();
+			}
+		} else {
+			$('.banner').show();
+		}
+	}
+	var $banner = $('.banner');
+	setInterval(function () {
+		if(currentChapter < 0 || currentChapter >= currentChapter.length - 1) {
+			return;
+		}
+
+		var newTime = video.currentTime;
+		$('#currentTime .value').text(newTime);
+		// Show banner when we're past the intro
+		if(newTime > 1.5) {
+			if(!$banner.hasClass('in')) {
+				$('.banner').fadeIn('fast').addClass('in');
+			}
+		} else {
+			if($banner.hasClass('in')) {
+				$('.banner').fadeOut('fast').removeClass('in');
+			}
+		}
+
+		if(newTime >= chapters[currentChapter].stop) {
+			video.pause();
+			video.currentTime = chapters[currentChapter].stop;
+			$('#video-controls').removeClass('dimmed');
+		}
+
+		var $activeItem = $('[data-seek].active');
+		$seekItems.each(function (i, item) {
+			var $item = $(item);
+			if(video.currentTime >= $item.data('seek')) {
+
+				if(!$activeItem.is($item)) {
+					$activeItem.removeClass('active');
+					$item.addClass('active');
+					$activeItem = $item;
+				}
+				return false;
+			}
+		});
+
+		var $activeItem = $('[data-chapter].active');
+		$chapterItems.each(function (i, item) {
+			var $item = $(item);
+			if(currentChapter <= $item.data('chapter')) {
+				if(!$activeItem.is($item)) {
+					$activeItem.removeClass('active');
+					$item.addClass('active');
+					$activeItem = $item;
+				}
+			}
+		});
+	}, 40);
+
+	var canPlay = false;
+	$('#container video').on('timeupdate', function (event) {
+
+	}).on('canplay', function () {
+		// Kill the loader
+		$('#loader').fadeOut();
+		// Show the begin CTA
+		$('#begin').removeClass('hidden').addClass('animated');
+		canPlay = true;
+	}).on(Modernizr.touch ? 'touchend' : 'click', function (event) {
+		if(!canPlay) {
+			return;
+		}
+		if(video.currentTime >= chapters[chapters.length - 1].start) {
+			$('#download_modal').modal('show');
+		} else {
+			if(video.paused && currentChapter < chapters.length) {
+				$('#begin').removeClass('fadeInUp animated').fadeOut();
+				currentChapter++;
+				video.play();
+				$('#video-controls').addClass('dimmed');
+				$('#prev.hidden, #next.hidden, #repeat.hidden').removeClass('hidden').addClass('animated');
+			}
+		}
+	});
+
+	onResize();
+
+	$('#container').on(Modernizr.touch ? 'touchend' : 'click', function () {
+		if($('#sidebar').hasClass('open')) {
+			$('#sidebar').removeClass('open');
+		}
+	}).imagesLoaded().always(repositionElements);
+
+	$('#contact_form').on('submit', function () {
+		var $form = $(this),
+			formData = {
+				first_name: $form.find('input[name=first_name]').val(),
+				last_name: $form.find('input[name=last_name]').val(),
+				email_address: $form.find('input[name=email_address]').val()
+			};
+
+		$.ajax('download.php', {
+			type: 'POST',
+			data: $form.serialize()
+		}).done(function () {
+			$.fileDownload('download.php');
+			$('#download_modal').modal('hide');
+		});
+
+		return false;
+	});
+
+	$('.menu-toggle').on(Modernizr.touch ? 'touchend' : 'click', function () {
+		$('#sidebar').toggleClass('open');
+	});
+
+	$('[data-seek]').on(Modernizr.touch ? 'touchend' : 'click', function () {
+		if(window.innerWidth >= 1200) {
+			video.currentTime = $(this).data('seek');
+		}
+	});
+
+	$('[data-chapter]').on(Modernizr.touch ? 'touchend' : 'click', function () {
+		if(window.innerWidth >= 1200) {
+			if(video.paused) {
+				currentChapter = $(this).data('chapter');
+				video.currentTime = chapters[currentChapter].start;
+				video.play();
+				$('#video-controls').addClass('dimmed');
+				$('#begin').removeClass('fadeInUp animated').fadeOut();
+				$('#prev, #next, #repeat').removeClass('hidden');
+			}
+		}
+	});
+
+	$('[data-scroll]').on(Modernizr.touch ? 'touchend' : 'click', function () {
+		if(window.innerWidth < 1200) {
+			var pct = $(this).data('scroll') / 15315,
+				amount = $('#mobile-infographic').height() * pct;
+			$('#container').scrollTo(amount, 300);
+		}
+	});
+
+	$('#repeat').on('click', function () {
+		if(video.paused && currentChapter >= 0) {
+			// currentChapter--;
+			video.currentTime = chapters[currentChapter].start;
+			video.play();
+			$('#video-controls').addClass('dimmed');
+		}
+	});
+	$('#prev').on('click', function () {
+		if(video.paused && currentChapter > 0) {
+			currentChapter--;
+			video.currentTime = chapters[currentChapter].start;
+			video.play();
+			$('#video-controls').addClass('dimmed');
+		}
+	});
+	$('#next').on('click', function () {
+		if(video.paused && currentChapter < chapters.length) {
+			currentChapter++;
+			video.play();
+			$('#video-controls').addClass('dimmed');
+		}
+	});
+	$('#begin').on('click', function () {
+		// $('#container .poster').slideUp('slow').removeClass('show');
+		// $('#begin').fadeOut();
+		$('#begin').removeClass('fadeInUp animated').fadeOut();
+		currentChapter++;
+		video.play();
+		$('#prev, #next, #repeat').removeClass('hidden').addClass('animated');
+		$('#video-controls').addClass('dimmed');
+	});
+	$('#container video').attr('preload','auto');
+	$('#container video')[0].play();
+	$('#container video')[0].pause();
+});
