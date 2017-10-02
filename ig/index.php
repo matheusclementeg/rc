29b684ecf9df3798d01e9f3ec634a049ed81d84a
Pedro Kfuri <pedrokfuri@outlook.com>
2017-10-02 19:16:31 -0300

    changed controls

--- ig/index.php
+++ ig/index.php
@@ -6,52 +6,57 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="Entenda o que é o Marketing de Conteúdo a partir desse super infográfico interativo!">
 	
-	<meta property="og:title" content="Entenda o que é Marketing de Conteúdo">
+	<!-- <meta property="og:title" content="Entenda o que é Marketing de Conteúdo">
 	<meta property="og:type" content="website">
 	<meta property="og:url" content="https://rockcontent.com">
-	<meta property="og:image" content="http://nativeadscience.com/img/social-image.png">
+	<meta property="og:image" content="http://nativeadscience.com/img/social-image.png"> -->
 	<meta property="og:description" content="Entenda o que é o Marketing de Conteúdo a partir desse super infográfico interativo!">
 
-	<link rel="icon" type="image/x-icon" href="favicon.ico">
+	<link rel="icon" type="image/x-icon" href="https://rockcontent.com/wp-content/themes/RockContent/favicon.png">
 
 	<title>O que é Marketing de Conteúdo?</title>
-	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link href="//cdnjs.cloudflare.com/ajax/libs/animate.css/3.3.0/animate.min.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 	<link href="css/loaders.min.css" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 	<link href="css/custom.css" rel="stylesheet">
+
 	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 
 </head>
 <body>
 
 	<div id="container">
-		<video src="motion-infographic.mp4" preload="auto" poster="img/poster.gif" style="max-height: 446px;"></video>
+
+		<video src="motion-infographic.mp4" preload="auto" poster="img/poster.gif" style="max-height: 446px;">
+		</video>
+		<div id="bar" class="col-lg-12">
+
+			<div class="col-lg-4"></div>
+			<div id="controles" class="col-lg-4">
+				<div id="antes">
+					<span id="prev" class="col-lg-4 glyphicon glyphicon-step-backward hidden fadeInLeft"></span>
+				</div>
+				<div id="repete">
+					<span id="repeat" class="col-lg-4 glyphicon glyphicon-repeat hidden fadeInLeft"></span>
+				</div>
+				<div id="depois">	
+					<span id="next" class="col-lg-4 glyphicon glyphicon-step-forward hidden fadeInRight"></span>
+				</div>
+			</div>
+
+		</div>
 		<div id="loader" style="display: none;">
 			<div class="text">Carregando...</div>
-		</div>
+		</div> 
 		<a id="begin" class="fadeInUp animated">
 			<div class="text">Clique para começar</div>
 		</a>
-		<div id="video-controls">
-			<a id="repeat" class="hidden fadeInLeft">
-				<img src="img/replay.png" width="65px">
-			</a>
-			<a id="prev" class="hidden fadeInLeft">
-				<img src="img/back.png" width="65px">
-			</a>
-			<a id="next" class="hidden fadeInRight">
-				<img src="img/next.png" width="65px">
-			</a>
-		</div>
+		
 		<img id="mobile-infographic" src="img/static-infographic.png">
 	</div>
-	<div id="aspect-warning">
-		<p>Maximize o tamanho de sua tela para melhorar a experiência. Caso esteja usando um dispositivo móvel, tente girar a tela para o modo paisagem.</p>
-	</div>
-	<div id="height-warning">
-		<p>A altura do seu navegador é muito pequena. Maximize a janela para melhor experiência.</p>
-	</div>
+
 	<script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/2.1.1/jquery.scrollTo.min.js"></script>
--- ig/js/custom.js
+++ ig/js/custom.js
@@ -1,27 +1,27 @@
 $(function () {
 	var chapters = [
-		{ start: 0, stop: 8.0 }, /*como era feito antes*/
-		{ start: 8.01, stop: 13.6 }, /*como funciona hoje*/
+		{ start: 0, stop: 7.95 }, /*como era feito antes*/
+		{ start: 7.96, stop: 13.6 }, /*como funciona hoje*/
 		{ start: 13.61, stop: 16.6 }, /*conteudo = atrair pessoas certas*/
 		{ start: 16.61, stop: 22.59 }, /*3 circulos de motivos*/
 		{ start: 22.60, stop: 25.49 }, /*estrategico, direcionado e reduz riscos*/
 		{ start: 25.50, stop: 28.6 }, /*conteudo custa até % menos*/
 		{ start: 28.61, stop: 30.35 }, /*gera mais oportunidade de venda*/
 		{ start: 30.36, stop: 31.2 }, /*como funciona?*/
-		{ start: 31.21, stop: 35.25 }, /*ima e pneu*/
+		{ start: 31.21, stop: 33.25 }, /*ima*/
+		{ start: 33.26, stop: 35.25 }, /*pneu*/
 		{ start: 35.26, stop: 37.1 }, /*eles se tornam clientes*/
 		{ start: 37.11, stop: 41.8 }, /*promotores da sua marca etc*/
 		{ start: 41.81, stop: 53.3 }, /*lucro economia clientes etc*/
 		{ start: 53.31, stop: 55.85 }, /*que tal comecar agora*/
 		{ start: 55.85, stop: 60.5 }, /*a rock content esta aqui pra ajudar*/
-		{ start: 60.5, stop: 70 }, /**/
-		{ start: 70.01, stop: 77 } /**/
+		{ start: 60.5, stop: 75}
 
 	];
 	var currentChapter = -1;
 	var video = $('#container video')[0],
 		$seekItems = $('[data-seek]').sort(function (a, b) {
-			return $(b).data('seek') - $(a).data('seek'); // Causes a reverse sort
+			return $(b).data('seek') - $(a).data('seek');
 		}),
 		$chapterItems = $('[data-chapter]').sort(function (a, b) {
 			return $(b).data('chapter') - $(a).data('chapter');
@@ -45,7 +45,7 @@ $(function () {
 			win_w = $(window).width();
 
 		$('#container video').css({
-			'max-height': win_h
+			'max-height': win_h - 40
 		});
 		$('#container .poster').css({
 			width: $('#container video').width(),
@@ -66,6 +66,7 @@ $(function () {
 			$('.banner').show();
 		}
 	}
+
 	var $banner = $('.banner');
 	setInterval(function () {
 		if(currentChapter < 0 || currentChapter >= currentChapter.length - 1) {
@@ -88,7 +89,7 @@ $(function () {
 		if(newTime >= chapters[currentChapter].stop) {
 			video.pause();
 			video.currentTime = chapters[currentChapter].stop;
-			$('#video-controls').removeClass('dimmed');
+			$('#controles').removeClass('dimmed');
 		}
 
 		var $activeItem = $('[data-seek].active');
@@ -138,7 +139,7 @@ $(function () {
 				$('#begin').removeClass('fadeInUp animated').fadeOut();
 				currentChapter++;
 				video.play();
-				$('#video-controls').addClass('dimmed');
+				$('#controles').addClass('dimmed');
 				$('#prev.hidden, #next.hidden, #repeat.hidden').removeClass('hidden').addClass('animated');
 			}
 		}
@@ -187,7 +188,7 @@ $(function () {
 				currentChapter = $(this).data('chapter');
 				video.currentTime = chapters[currentChapter].start;
 				video.play();
-				$('#video-controls').addClass('dimmed');
+				$('#controles').addClass('dimmed');
 				$('#begin').removeClass('fadeInUp animated').fadeOut();
 				$('#prev, #next, #repeat').removeClass('hidden');
 			}
@@ -204,10 +205,9 @@ $(function () {
 
 	$('#repeat').on('click', function () {
 		if(video.paused && currentChapter >= 0) {
-			// currentChapter--;
 			video.currentTime = chapters[currentChapter].start;
 			video.play();
-			$('#video-controls').addClass('dimmed');
+			$('#controles').addClass('dimmed');
 		}
 	});
 	$('#prev').on('click', function () {
@@ -215,24 +215,22 @@ $(function () {
 			currentChapter--;
 			video.currentTime = chapters[currentChapter].start;
 			video.play();
-			$('#video-controls').addClass('dimmed');
+			$('#controles').addClass('dimmed');
 		}
 	});
 	$('#next').on('click', function () {
 		if(video.paused && currentChapter < chapters.length) {
 			currentChapter++;
 			video.play();
-			$('#video-controls').addClass('dimmed');
+			$('#controles').addClass('dimmed');
 		}
 	});
 	$('#begin').on('click', function () {
-		// $('#container .poster').slideUp('slow').removeClass('show');
-		// $('#begin').fadeOut();
 		$('#begin').removeClass('fadeInUp animated').fadeOut();
 		currentChapter++;
 		video.play();
 		$('#prev, #next, #repeat').removeClass('hidden').addClass('animated');
-		$('#video-controls').addClass('dimmed');
+		$('#controles').addClass('dimmed');
 	});
 	$('#container video').attr('preload','auto');
 	$('#container video')[0].play();
